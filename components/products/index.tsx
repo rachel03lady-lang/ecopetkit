0cf31ddc6e5a238184be20d6eb7ba0a05d91d54a
Ravindra Kamble <write2ravi14@gmail.com>
2026-01-04 20:32:13 +0530

    staging site-fixe2

--- components/products/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as ProductPage } from "./product-page/ProductPage-bak.tsx";
-
-export { default as IndustryInsights } from "./industry-insights/IndustryInsights";
-export { default as ProductFAQ } from "./productFAQ/ProductFAQ";
